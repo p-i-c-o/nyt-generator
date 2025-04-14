aa5eae07ca6fd24446807690a607e66a44e99eaf
p-i-c-o <elmoelmojr@gmail.com>
2025-04-14 08:45:33 +0200

    inital

---
+++ build.sh
@@ -0,0 +1,12 @@
+jpackage \
+--input /home/elie/Java/NetBeans/nytCrosswordSelector/. \
+--name NYT \
+--main-jar /home/elie/Java/NetBeans/nytCrosswordSelector/target/nytCrosswordSelector-1.0-SNAPSHOT.jar \
+--main-class monnickendam.com.nytcrosswordselector.main \
+--type DEB \
+--linux-shortcut \
+--linux-deb-maintainer elie@monnickendam.ch \
+--icon /home/elie/Java/NetBeans/nytCrosswordSelector/mini.png \
+--linux-app-release 1.0.0 \
+--install-dir /opt/NYT \
+--verbose
---
+++ src/main/java/monnickendam/com/nytcrosswordselector/main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package monnickendam.com.nytcrosswordselector;
+
+/**
+ *
+ * @author elie
+ */
+public class main {
+    public static void main(String[] args) {
+    java.awt.EventQueue.invokeLater(new Runnable() {
+        public void run() {
+            new mainPage().setVisible(true);  // Replace with your JFrame class name
+        }
+    });
+}
+
+    
+}
